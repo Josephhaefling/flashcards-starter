6695ffccf19907de94970742f508d0ac0cbe1c29
Joe Haefling <josephhaefling@gmail.com>
2020-05-05 09:06:56 -0600

    Create deck class, tests and methods

---
+++ test/Deck-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+beforeEach(() => {
+  card1 = new Card(1, 'Can I?', ['yes', 'no', "don't talk to me"], 'no')
+  card2 = new Card(2, 'Huh?', ['what?', 'huh?', 'did you say something?'], 'no')
+  card3 = new Card(3, 'Whats your favorite color?', ['red', 'blue', 'green', 'black'], 'green')
+  deck = new Deck([card1, card2, card3])
+})
+
+describe('Deck', () => {
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function')
+  });
+  it('should know how many cards are in the deck', () => {
+    expect(deck.getNumberOfCards()).to.equal(3)
+  })
+})
